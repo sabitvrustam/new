2ace39db0d6c3cef8707a99d83b90b0e4b03168d
Рустам <rustam@MacBook-Pro-Voronkin.local>
2022-12-27 23:00:24 +0400

    refactoring js

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -1,3 +1,26 @@
+// var test = document.getElementById('test');
+// console.log(test.id);
+// test.innerHTML = "Привет друг";
+
+// var newOrder = document.getElementById('newOrder');
+// console.log(newOrder.method);
+
+function newUserForm (el){
+    var login = el.login.value;
+    var password = el.password.value;
+    var passwoerdCon = el.passwordCon.value;
+    console.log(login, password, passwoerdCon);
+    if (login == "" || login.lenght < 3){
+
+        document.getElementById("loginErr").innerHTML = "Не корректный логин";
+       // window.location = 'https://www.google.com';
+        return false;
+    }
+
+    return false;
+}
+
+
 function createOrder (el){
 el.innerHTML = "hihihi";
 
